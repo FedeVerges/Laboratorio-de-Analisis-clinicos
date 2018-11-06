de76ae7ca5ac0af5e171c5d32b36a591a69608a4
fede_ <fede_@DESKTOP-KSBERJ4>
2018-11-06 20:55:12 -0300

    se agregó clases para la implementacion del patrón de diseño DAO

---
+++ src/DAO_Sqlite/DAOPacientes.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO_Sqlite;
+
+/**
+ *
+ * @author fede_
+ */
+public class DAOPacientes implements DAO{
+
+    @Override
+    public void create() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void read() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
